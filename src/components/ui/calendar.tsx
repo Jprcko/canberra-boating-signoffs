b744a20fee8d807de5acbc489547ef989f5ae674
Jprcko <jisan.rafi@gmail.com>
2025-07-02 01:08:06 +1000

    Update calendar.tsx
    chat gpt

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -2,7 +2,7 @@
 import * as React from "react";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import { DayPicker } from "react-day-picker";
-import { enGB } from "date-fns/locale";
+import { enGB } from "react-day-picker/locale";
 import { cn } from "@/lib/utils";
 import { buttonVariants } from "@/components/ui/button";
 
@@ -20,7 +20,6 @@ function Calendar({
     <DayPicker
       showOutsideDays={showOutsideDays}
       locale={locale}
-      weekStartsOn={weekStartsOn}
       className={cn("p-3 pointer-events-auto", className)}
       classNames={{
         months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
